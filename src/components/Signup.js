162e87c2515bfcb4f806eb48632146526b180c18
ethanalvizo <55671206+ethanalvizo@users.noreply.github.com>
2021-04-13 17:06:57 -0400

    Implemented signup feature

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,16 +1,41 @@
-import React, { useRef } from 'react';
-import { Card, Form, Button } from 'react-bootstrap'
+import React, { useRef, useState } from 'react';
+import { Card, Form, Button, Alert } from 'react-bootstrap';
+import { useAuth } from '../contexts/AuthContext'
 
 export default function Signup() {
     const emailRef = useRef();
     const passwordRef = useRef();
     const passwordConfirmRef = useRef();
+    const { signup } = useAuth();
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        if(passwordRef.current.value !== passwordConfirmRef.current.value) {
+            return setError('Passwords do not match')
+        }
+
+        try {
+            setError('');
+            setLoading(true);
+            await signup(emailRef.current.value, passwordRef.current.value)
+        }   catch {
+            setError('Failed to create an account')
+        }
+
+        setLoading(false)
+
+    }
+
     return (
         <>
         <Card>
             <Card.Body>
                 <h2 className="text-center mb-4">Sign Up</h2>
-                <Form>
+                {error && <Alert variant="danger">{error}</Alert>}
+                <Form onSubmit={handleSubmit}>
                     <Form.Group id="email">
                         <Form.Label>Email</Form.Label>
                         <Form.Control type="email" ref={emailRef} require />
@@ -19,7 +44,7 @@ export default function Signup() {
                         <Form.Label>Confirm Password</Form.Label>
                         <Form.Control type="password" ref={passwordConfirmRef} require />
                     </Form.Group>
-                    <Button className="w-100" type="submit">Sign Up</Button>
+                    <Button disabled={loading} className="w-100" type="submit">Sign Up</Button>
                 </Form>
             </Card.Body>
         </Card>
